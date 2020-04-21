8e1c2a900e0bc4a48b4263019ad7dfaa81751f1a
gichelli <ggonza18@masonlive.gmu.edu>
2020-04-21 17:19:16 -0400

    changing test script jja

--- testscript.sh
+++ testscript.sh
@@ -2,6 +2,6 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "I AM SOO GREATtttttttttttttiiiiiiiiiiii raya"
+echo "I AM SOO GREAT raya"
 echo "I AM THE MOST BEAUTIFULl GIRL IN THE WORLD"
 echo "My name is GISSELLITA"
